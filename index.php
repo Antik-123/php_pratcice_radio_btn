121eeb04672f75abfe47b9cba2bcd4272b06a84e
Antik-123 <antikmahmud99@gmail.com>
2025-08-29 23:35:38 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <!-- <form action="./index.php" method="post">
+    <label for="">username: </label> <br>
+    <input type="text" name="username"> <br>
+    <label for="">password: </label> <br>
+    <input type="password" name="password"> <br>
+    <input type="submit" name="login" value="log in"> <br>
+
+  </form> -->
+
+
+  <form action="./index.php" method="post">
+    <input type="radio" name="creadit_card"  value="visa">
+    visa <br>
+    <input type="radio" name="credit_card" value="mastercard">
+    mastercard <br>
+    <input type="radio" name="credit_card" value="American Express">
+    American Express <br>
+    <input type="submit" name="confirm" value="confirm">
+  </form>
+</body>
+</html>
+
+ <?php
+
+  //  if(isset($_POST["login"])){
+  //   $username = $_POST["username"];
+  //   $password = $_POST["password"];
+
+  //   if(empty($username)){
+  //     echo "user name is missing";
+  //   }
+  //   elseif(empty($password)){
+  //     echo "user password is missing";
+  //   }
+  //   else{
+  //     echo "hello {$username}";
+  //   }
+  //  }
+
+  if(isset($_POST["confirm"])){
+    $credit_card = null;
+
+    if(isset($_POST["credit_card"])){
+       $credit_card = $_POST["credit_card"];
+      
+
+    }
+    if($credit_card == "visa"){
+      echo "you selected visa";
+    }
+    elseif($credit_card == "mastercard"){
+      echo "you selected visa";
+    }
+    elseif($credit_card == "American Express"){
+      echo "you selected American Express";
+    }
+    else{
+      echo "please make a selection";
+    }
+   
+    
+  }
+
+
+
+
+
+
+
+
+
+                  
+
+
+             
+
+     
+
+ ?>
